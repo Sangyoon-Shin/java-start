e06244071c4eb9b527ae2a997aea5c16c414bb2d
sangyoon_2.23 <tlstkddbs223@naver.com>
2025-11-26 20:25:30 +0900

    Initial commit

---
+++ src/Comment.java
@@ -0,0 +1,14 @@
+public class Comment {
+
+    // 주석 쓰는 건 C언어랑 동일하다.
+    // '//' 혹은 '/* */'
+    // psvm => 자동으로 public static void main(String[] args) 구문 만들어준다.
+
+    public static void main(String[] args){
+        // sout => System.out.println() 자동완성.
+        System.out.println("hello world java1"); // hello java1을 출력합니다.
+        // System.out.println("hello world java2");
+    }
+
+
+}
---
+++ src/HelloJava.java
@@ -0,0 +1,8 @@
+public class HelloJava{ // 클래스의 범위 시작
+
+    // 주의! Java 언어는 대소문자 구분한다. 대소문자 다르면 오류 발생. String, System 등 유의해서 작성하자.
+    public static void main(String[] args){ // 프로그램의 시작점. main 메서드의 범위 시작.
+        System.out.println("Hello Java"); // 값을 콘솔에 출력. 자바 문자열 => 쌍따옴표로 묶기.
+    } // main 메서드의 범위 끝.
+
+} // 클래스의 범위 끝
