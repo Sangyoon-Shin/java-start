7053a7a1bb2a61bf127654a30715ce63f14300e0
sangyoon_2.23 <tlstkddbs223@naver.com>
2025-11-28 22:05:32 +0900

    연산자 개념 + 예제

---
+++ src/operator/ex/OperationEx1.java
@@ -0,0 +1,11 @@
+package operator.ex;
+
+public class OperationEx1 {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 30;
+        int sum = a + b + c;
+        int average = sum / 3;
+        System.out.println(sum);
+        System.out.println(average);
+    }
+}
